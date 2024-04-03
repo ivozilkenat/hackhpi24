41e91fbfe7f610763f65a761642a055e1963eece
Ivo Zilkenat <44005173+ivozilkenat@users.noreply.github.com>
2024-04-03 12:05:13 +0200

    Update start_client.py

--- start_client.py
+++ start_client.py
@@ -1,7 +1,7 @@
 from _utilities import *
             
 def start_client(path=os.path.join(os.getcwd(), "client")):
-    run_command(f'cd {path} && npm start')
+    run_command(f'cd {path} && npm start -- --port 80')
 
 if __name__ == "__main__":
     start_client()
