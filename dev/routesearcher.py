ade4eb55c5730468dadffc6bb21819cc0bfa66e7
Valentin Teutschbein <mail@valteu.com>
2024-04-18 17:40:43 +0200

    refactored routesearcher.py

--- dev/routesearcher.py
+++ dev/routesearcher.py
@@ -1,9 +1,6 @@
 import requests
 
 def get_routeData(src, dst):
-    #url = f"https://hackhpi24.ivo-zilkenat.de/api/trips/"
-    #url = f"http://localhost:3001/api/trips/"
-    #data = {"bounds": {"upper-left": {"lat": 40.7128,"lon": -74.006}, "lower-right": {"lat": 40.7128,"lon": -74.006}}}
     prequel = 'https://v6.vbb.transport.rest/journeys?'
     url = f'{prequel}from={src}&to={dst}&polylines=true&subStops=false&entrances=false&remarks=false&results=1'
     response = requests.get(url)
